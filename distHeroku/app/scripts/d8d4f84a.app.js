02332276238d617ccc2d96b6b0a2ba45bb6cac49
Slomic, Ismar <ismar.slomic@accenture.com>
2013-09-28 22:07:39 +0200

    Updated Grunt.js so it creates complied code into the distHeroku. This folder will be pushed to Heroku. Added bootstrap to the /app

---
+++ distHeroku/app/scripts/d8d4f84a.app.js
@@ -0,0 +1,10 @@
+'use strict';
+angular.module('adsCmsFrontendApp', []).config([
+  '$routeProvider',
+  function ($routeProvider) {
+    $routeProvider.when('/', {
+      templateUrl: 'views/main.html',
+      controller: 'MainCtrl'
+    }).otherwise({ redirectTo: '/' });
+  }
+]);
