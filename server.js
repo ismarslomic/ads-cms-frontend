a5dc57b7048c7efa5ff186e453f0e944ec5824a6
Slomic, Ismar <ismar.slomic@accenture.com>
2013-10-06 11:56:50 +0200

    Added grunt watch on changes in jade files and automatically refreshing in web browser

---
+++ server.js
@@ -0,0 +1,21 @@
+var gzippo = require('gzippo'),
+    express = require('express'),
+    path = require('path'),
+    app = express();
+
+// Start Exoress server
+app.set('port', process.env.PORT || 5000);
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+// below is not on use when using res.sendfile
+app.use(gzippo.staticGzip(path.join(__dirname, 'dist')));
+app.listen(app.get('port'));
+console.log("Server is listening on port " + app.get('port'));
+
+
+// Route "/" to app-/index.html
+app.get('/', function (req, res) {
+    res.sendfile('./dist/index.html');
+});
